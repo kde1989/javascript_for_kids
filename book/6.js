b218368bb9593047db348c7227007c0ab0108921
kde1989 <k.d.e.1989@gmail.com>
2023-03-15 16:30:26 +0300

    Run!

---
+++ book/6.js
@@ -0,0 +1,6 @@
+var codeWar1 = "обернись";
+var codeWar2 = "неужели";
+var codeWar3 = "огурцы";
+var codeWar4 = "липкие";
+var codeWar5 = "?!";
+codeWar1[1] + codeWar2[1] + codeWar3[1] + codeWar4[1] + codeWar5[1];
