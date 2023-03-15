4487069eb129a9f8d62337f80ce38324deb04346
kde1989 <k.d.e.1989@gmail.com>
2023-03-15 12:04:04 +0300

    secondsInADenis

---
+++ book/5.js
@@ -0,0 +1,17 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var secondsInAHour = secondsInAMinute * minutesInAHour;
+secondsInAHour;
+3600
+var hoursInADay = 24;
+var secondsInADay = secondsInAHour * hoursInADay;
+secondsInADay;
+86400
+var daysInAYear = 365;
+var secondsInAYear = daysInAYear * secondsInADay;
+secondsInAYear;
+31536000
+var ageDenis = 34;
+var secondsInADenis = ageDenis * secondsInAYear;
+secondsInADenis;
+1072224000
