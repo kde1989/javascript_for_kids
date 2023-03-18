b9ab44f850723512bfb50c2acf4f780ed430b5ff
kde1989 <k.d.e.1989@gmail.com>
2023-03-18 15:51:03 +0300

    randomInsult 2

---
+++ book/12.js
@@ -0,0 +1,12 @@
+var randomBodyParts = ["рот", "нос", "глаз"];
+var randomAdjectives = ["смешной", "огромный", "вонючий"];
+var randomEnimals = ["слона", "муравья", "слизняка", "дятла", "голубя"];
+var randomBodyEnimals = ["хобот", "ус", "попа", "клюв", "хвост"];
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+var randomEnimal = randomEnimals[Math.floor(Math.random() * 5)];
+var randomBodyEnimal = randomBodyEnimals[Math.floor(Math.random() * 5)];
+var randomInsult = "У тебя " + randomBodyPart + " ещё более " + randomAdjective + ", чем " + randomBodyEnimal + " у " + randomEnimal + "!!!" + " ;)";
+randomInsult;
+
+'У тебя глаз ещё более смешной, чем хобот у муравья!!! ;)'
