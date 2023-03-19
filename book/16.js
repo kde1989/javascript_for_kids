a26eeeb00cd8d27e76590118acce358ba7be1644
kde1989 <k.d.e.1989@gmail.com>
2023-03-19 15:06:01 +0300

    scores

---
+++ book/16.js
@@ -0,0 +1,11 @@
+var scores = {
+    Den: 3,
+    Nick: 0,
+    Anna: 5,
+    Bob: 9,
+    Vera: 1
+}
+scores["Den"] += 9;
+scores["Anna"] += 8;
+scores;
+{Den: 12, Nick: 0, Anna: 13, Bob: 9, Vera: 1}
