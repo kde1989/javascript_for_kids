24152245b510326273711f097ed764fb09e4adbb
kde1989 <k.d.e.1989@gmail.com>
2023-03-19 15:14:57 +0300

    myCrazyObject

---
+++ book/17.js
@@ -0,0 +1,7 @@
+var myCrazyObject = {
+    "name": "Нелепый объект",
+    "some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],
+    "random enimal": "Банановая акула"
+};
+myCrazyObject["some array"][2]["number"];
+123
