b1cfa11cbdaa732d575616917a58fb6e25834e5f
kde1989 <k.d.e.1989@gmail.com>
2023-03-20 18:31:28 +0300

    "Меньше 10000"

---
+++ book/19.js
@@ -0,0 +1,9 @@
+for (let i = 3; i < 10000; i = i * 3) {
+    console.log(i);
+}
+let threeS = 3;
+while (threeS < 10000) {
+    console.log(threeS);
+    threeS = threeS * 3;
+}
+console.log("Меньше 10000");
