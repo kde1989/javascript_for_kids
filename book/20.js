85d915f31abeb4cc3c92ffa6eeacc1395bb8b644
kde1989 <k.d.e.1989@gmail.com>
2023-03-20 18:39:22 +0300

    "  - прекрасное животное"

---
+++ book/20.js
@@ -0,0 +1,6 @@
+let animals = ["Кот", "Рыба", "Лемур", "Комодский варан"];
+animals[0] = animals[0] + "  - прекрасное животное";
+animals[1] = animals[1] + "  - прекрасное животное";
+animals[2] = animals[2] + "  - прекрасное животное";
+animals[3] = animals[3] + "  - прекрасное животное";
+console.log(animals);
