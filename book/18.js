4a03e7b62af17d9b8474753dbadab130b5b6e26d
kde1989 <k.d.e.1989@gmail.com>
2023-03-20 15:42:18 +0300

    Привет мне!

---
+++ book/18.js
@@ -0,0 +1,10 @@
+let name = "Denis";
+    if (name === "Denis") {
+        console.log("Привет мне!");
+    } else if (name === "Евгений") {
+        console.log("Привет, папа!");
+    } else if (name === "Елена") {
+        console.log("Привет, мама!");
+    } else {
+        console.log("Привет, незнакомец!");
+    }
