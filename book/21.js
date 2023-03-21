466680beff9944d99d6e2f7d172c6d1e486ce89c
kde1989 <k.d.e.1989@gmail.com>
2023-03-21 15:02:05 +0300

    randomString

---
+++ book/21.js
@@ -0,0 +1,14 @@
+var alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+var randomString = "";
+while (randomString.length < 6) {
+    console.log(randomString += alphabet[Math.floor(Math.random() * alphabet.length)]);
+}
+console.log(randomString);
+VM3171:4 я
+VM3171:4 яю
+VM3171:4 яюу
+VM3171:4 яюуф
+VM3171:4 яюуфр
+VM3171:4 яюуфрн
+VM3171:6 яюуфрн
+undefined
