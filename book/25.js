51ea08efb948f4357d6ea47d1126a4c860681feb
kde1989 <k.d.e.1989@gmail.com>
2023-03-23 18:15:53 +0300

    Виселица!

--- book/25.js
+++ book/25.js
@@ -1,7 +1,25 @@
-var words = ["программа", "макака", "прекрасный", "оладушек"];
-var word = words[Math.floor(Math.random() * words.length)];
-var answerArray = [];
-for(var i = 0; i < word.length; i++) {
+let words = ["программа", "макака", "прекрасный", "оладушек"];
+let word = words[Math.floor(Math.random() * words.length)];
+let answerArray = [];
+for(let i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
-var remainingLetters = word.length;
+let remainingLetters = word.length;
+while(remainingLetters > 0) {
+    alert(answerArray.join(" "));
+    let guess = prompt("Угадайте букву, или нажмите Отмена для выхода из игры.");
+    if(guess === null) {
+        break;
+    } else if(guess.length !== 1) {
+        alert("Пожалуйста, введите одиночную букву");
+    } else {
+        for(let j = 0; j < word.length; j++) {
+            if(word[j] === guess) {
+                answerArray[j] = guess;
+                remainingLetters--;
+            }
+        }
+    }
+}
+alert(answerArray.join (" "));
+alert ("Отлично! Было загадано слово " + word);
