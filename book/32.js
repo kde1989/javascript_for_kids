2293233c1352f6bdee497bee9d0799411fe4c24c
kde1989 <k.d.e.1989@gmail.com>
2023-03-28 13:57:41 +0300

    randomString (return)

---
+++ book/32.js
@@ -0,0 +1,8 @@
+let generateRandomInsult = function () {
+    let randomBodyParts = ["глаз", "нос", "череп"];
+    let randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+    let randomWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+    let randomString = "У тебя " + pickRandomWord(randomBodyParts) + " словно " + pickRandomWord(randomAdjectives) + " " + pickRandomWord(randomWords) + "!!! ;))";
+    return randomString;
+};
+generateRandomInsult();
