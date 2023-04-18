8f4f11f1e103c1e62ec1815101db2443c4034755
kde1989 <k.d.e.1989@gmail.com>
2023-04-18 13:06:37 +0300

    dog

---
+++ book/37.js
@@ -0,0 +1,18 @@
+let dog = {
+    name: "Оладушек",
+    legs: 4,
+    isAwesome: true
+};
+undefined
+dog.name;
+'Оладушек'
+dog.legs;
+4
+dog.isAwesome;
+true
+dog.age = 6;
+6
+dog.age;
+6
+dog;
+{name: 'Оладушек', legs: 4, isAwesome: true, age: 6}
